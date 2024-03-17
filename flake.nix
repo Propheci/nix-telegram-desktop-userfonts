4543fa7165b16006c528be57def27f6e08fa609c
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-03-17 20:12:41 +0530

    [flake] initial flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+    description = "Telegram Desktop Userfonts";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+        telegram-desktop-userfonts = pkgs.telegram-desktop.overrideAttrs (old: {
+            preConfigure = ''
+                for ttf in Telegram/lib_ui/fonts/*.ttf; do
+                    rm $ttf
+                    touch $ttf
+                done
+                sed -i 's/DemiBold/Bold/g' Telegram/lib_ui/ui/style/style_core_font.cpp
+            '';
+
+            cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
+        });
+    };
+}
