a317593f464cdff780b62f9d90c3aad428e42b25
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-03-18 17:19:08 +0530

    [flake] fix flake output

--- flake.nix
+++ flake.nix
@@ -10,16 +10,18 @@
         pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-        telegram-desktop-userfonts = pkgs.telegram-desktop.overrideAttrs (old: {
-            preConfigure = ''
-                for ttf in Telegram/lib_ui/fonts/*.ttf; do
-                    rm $ttf
-                    touch $ttf
-                done
-                sed -i 's/DemiBold/Bold/g' Telegram/lib_ui/ui/style/style_core_font.cpp
-            '';
+        packages = {
+            telegram-desktop-userfonts = pkgs.telegram-desktop.overrideAttrs (old: {
+                preConfigure = ''
+                    for ttf in Telegram/lib_ui/fonts/*.ttf; do
+                        rm $ttf
+                        touch $ttf
+                    done
+                    sed -i 's/DemiBold/Bold/g' Telegram/lib_ui/ui/style/style_core_font.cpp
+                '';
 
-            cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
-        });
+                cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
+            });
+        };
     };
 }
