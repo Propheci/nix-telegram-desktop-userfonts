947fed657c765c960b1ac38bb3c9d9f5aa3262a5
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-04-04 21:35:42 +0530

    [flake] build for x86_64-linux and aarch-linux

--- flake.nix
+++ flake.nix
@@ -3,15 +3,23 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+        systems.url = "github:nix-systems/default-linux";
     };
 
-    outputs = { self, nixpkgs }:
+    outputs = { self, nixpkgs, systems }:
     let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        inherit (nixpkgs) lib;
+        eachSystem = lib.genAttrs (import systems);
+        pkgsFor = eachSystem (system:
+            import nixpkgs {
+                localSystem = system;
+            }
+        );
     in
     {
-        packages = {
-            telegram-desktop-userfonts = pkgs.telegram-desktop.overrideAttrs (old: {
+        packages = eachSystem (system: {
+            telegram-desktop-userfonts = pkgsFor."${system}".telegram-desktop.overrideAttrs (old: {
                 preConfigure = ''
                     for ttf in Telegram/lib_ui/fonts/*.ttf; do
                         rm $ttf
@@ -22,6 +30,6 @@
 
                 cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
             });
-        };
+        });
     };
 }
