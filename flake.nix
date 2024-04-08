491af08da7f089f581d5dead4016b5b9ea45c1bb
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-04-08 12:17:36 +0530

    [flake] add an overlay for the package

--- flake.nix
+++ flake.nix
@@ -15,35 +15,41 @@
                 localSystem = system;
             }
         );
+        telegramOverride = (old: {
+            pname = "telegram-desktop-userfonts";
+
+            preConfigure = ''
+                for ttf in Telegram/lib_ui/fonts/*.ttf; do
+                    rm $ttf
+                    touch $ttf
+                done
+                sed -i 's/DemiBold/Bold/g' Telegram/lib_ui/ui/style/style_core_font.cpp
+            '';
+
+            cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
+
+            meta = with lib; {
+                description = "Telegram Desktop which respects fontconfig";
+                longDescription = ''
+                  Desktop client for the Telegram messenger, based on the Telegram API and
+                  the MTProto secure protocol.
+                '';
+                license = licenses.gpl3Only;
+                platforms = platforms.all;
+                homepage = "https://desktop.telegram.org/";
+                changelog = "https://github.com/telegramdesktop/tdesktop/releases/tag/v${old.version}";
+                mainProgram =  old.mainProgram;
+            };
+        });
     in
     {
-        packages = eachSystem (system: {
-            default = pkgsFor."${system}".telegram-desktop.overrideAttrs (old: {
-                pname = "telegram-desktop-userfonts";
-
-                preConfigure = ''
-                    for ttf in Telegram/lib_ui/fonts/*.ttf; do
-                        rm $ttf
-                        touch $ttf
-                    done
-                    sed -i 's/DemiBold/Bold/g' Telegram/lib_ui/ui/style/style_core_font.cpp
-                '';
-
-                cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_BUILD_TYPE=Release" ];
-
-                meta = with lib; {
-                    description = "Telegram Desktop which respects fontconfig";
-                    longDescription = ''
-                      Desktop client for the Telegram messenger, based on the Telegram API and
-                      the MTProto secure protocol.
-                    '';
-                    license = licenses.gpl3Only;
-                    platforms = platforms.all;
-                    homepage = "https://desktop.telegram.org/";
-                    changelog = "https://github.com/telegramdesktop/tdesktop/releases/tag/v${old.version}";
-                    mainProgram =  old.mainProgram;
-                };
+        overlays = {
+            default = (final: prev: {
+                telegram-desktop = prev.telegram-desktop.overrideAttrs (telegramOverride);
             });
+        };
+        packages = eachSystem (system: {
+            default = pkgsFor."${system}".telegram-desktop.overrideAttrs (telegramOverride);
         });
     };
 }
